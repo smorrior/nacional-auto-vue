734340efc158286e6da3dbdb69739ef78b803a52
stefan.jelinovic <stefan.jelinovic@mozzartbet.com>
2024-11-20 22:59:15 +0100

    Deploy to GitHub Pages

---
+++ assets/AboutView-M4LzBNxY.js
@@ -0,0 +1,1 @@
+import{_ as t,c as s,a as o,o as a}from"./app-CcxtoYpa.js";const n={},c={class:"about"};function r(_,e){return a(),s("div",c,e[0]||(e[0]=[o("h1",null,"This is an about page test",-1)]))}const l=t(n,[["render",r]]);export{l as default};
