8ac2e5d2c895fceafeb8986e6ed6eee6a8e3df88
stefan.jelinovic <stefan.jelinovic@mozzartbet.com>
2024-11-21 10:39:13 +0100

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,13 @@
-import HomeView from '@/views/HomeView.vue';
-import AboutView from '@/views/AboutView.vue';
-
 
 export const router = [
   {
     path: '/',
     name: 'home',
-    component: HomeView,
+    component: () => import('../views/HomeView.vue'),
   },
   {
     path: '/o-nama',
     name: 'about',
-    component: AboutView,
+    component: () => import('../views/AboutView.vue'),
   },
 ];
